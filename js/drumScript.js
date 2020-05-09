1bd3ca7c93804ce68b9486e459eb7fecd38632ef
ROB1-alt <robin.pautigny@hetic.net>
2020-05-09 16:48:25 +0200

    📣create a guideline for user📣

--- js/drumScript.js
+++ js/drumScript.js
@@ -1,6 +1,15 @@
 const soundVolume = document.querySelector('.volumeConfig')
 console.log(soundVolume.value / 10)
 
+//var slider = document.getElementById("myRange");
+//var output = document.getElementById("demo");
+//output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+//slider.oninput = function() {
+  //output.innerHTML = this.value;
+
+
 function Sound (id){
     let sound = document.getElementById(id)
     sound.volume = soundVolume.value / 10
@@ -21,5 +30,3 @@ document.addEventListener("keydown", function(event) {
         break
    }
        })
-
-       getElementById
