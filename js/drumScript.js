ffe1f9a64c95d00c63bee78cad136ed2eb9fd2ce
ROB1-alt <robin.pautigny@hetic.net>
2020-05-09 15:45:55 +0200

    add my JS

--- js/drumScript.js
+++ js/drumScript.js
@@ -1,3 +1,25 @@
 const soundVolume = document.querySelector('.volumeConfig')
 console.log(soundVolume.value / 10)
 
+function Sound (id){
+    let sound = document.getElementById(id)
+    sound.volume = soundVolume.value / 10
+    if (sound.paused){
+    sound.play()
+    } else {
+    sound.currentTime = 0
+    }
+}
+
+
+
+document.addEventListener("keydown", function(event) {
+   let eventWichKey = event.which
+   switch (eventWichKey){
+       case 65:
+           Sound ('firstSound')
+        break
+   }
+       })
+
+       getElementById
