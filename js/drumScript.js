a3f06fd5baed69c6654b0e93c04038f591810ab9
ROB1-alt <robin.pautigny@hetic.net>
2020-05-09 11:35:34 +0200

    fix my soundbar and add some JS for the work🧠

--- js/drumScript.js
+++ js/drumScript.js
@@ -1,8 +1,3 @@
 const soundVolume = document.querySelector('.volumeConfig')
 console.log(soundVolume.value / 10)
 
-document.addEventListener("keydown", function(event) {
-    let eventWichKey = event.which
-    switch (eventWichKey) {
-        case 65:
-            playSound('.1st',1)
